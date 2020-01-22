6de6eb6e75e818f8bdb9a2c5d855b2a758daf3b6
Eamon Bracht <ebracht2@illinois.edu>
2020-01-22 15:26:05 -0600

    fixed nan

--- composition.py
+++ composition.py
@@ -87,7 +87,7 @@ for count, area in enumerate(data):
         continue
     relations = get_relations(opt, area)
     area_tensor = (np.nan_to_num(area))
-    area_tensor = torch.from_numpy(area).type(torch.FloatTensor).unsqueeze(2)
+    area_tensor = torch.from_numpy(area_tensor).type(torch.FloatTensor).unsqueeze(2)
     if(opt.nt_data != opt.nt):
         area_tensor = area_tensor[opt.nt_data-opt.nt:]
     print("area tensor", area_tensor.shape)
